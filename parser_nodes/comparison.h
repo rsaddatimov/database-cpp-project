c0789cf0653223bb005456efd8b91a1afaa9c9e2
rsaddatimov <rafa.saddatimov@gmail.com>
2020-07-29 03:45:10 +0300

    added template Compare function

--- parser_nodes/comparison.h
+++ parser_nodes/comparison.h
@@ -8,3 +8,23 @@ enum class Comparison {
     Equal,
     NotEqual
 };
+
+template<class ValueType>
+bool Compare(const ValueType& lhs, const ValueType& rhs, Comparison cmp) {
+    switch (cmp) {
+    case Comparison::Less:
+        return lhs < rhs;
+    case Comparison::LessOrEqual:
+        return lhs <= rhs;
+    case Comparison::Greater:
+        return lhs > rhs;
+    case Comparison::GreaterOrEqual:
+        return lhs >= rhs;
+    case Comparison::Equal:
+        return lhs == rhs;
+    case Comparison::NotEqual:
+        return lhs != rhs;
+    default:
+        return true;
+    }
+}
